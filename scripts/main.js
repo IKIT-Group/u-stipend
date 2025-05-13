f7824bf2c42cbbf38424664d1e3ff42c7efaabbf
Dmitry Stupin <dm.stup@mail.ru>
2025-05-13 17:18:39 +0700

    адаптив сайта и мелкие доработки

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 import Header from "./Header.js";
 
 new Header()
+
+const resultTitle = document.getElementById("resultTitle")
+const resultPopup = document.getElementById("resultPopup")
+
+resultTitle.addEventListener("click", () => {
+  resultPopup.classList.toggle("visually-hidden")
+})
